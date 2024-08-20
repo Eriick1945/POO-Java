ef126e8f677ba3f9eb2b5704d869e9d63b330390
Eriick1945 <162646349+Eriick1945@users.noreply.github.com>
2024-08-20 22:02:00 +0000

    lista01 POO(atividade)

---
+++ Lista01/src/Main.java
@@ -0,0 +1,24 @@
+import classes.Livros;
+
+public class Main {
+    public static void main(String[] args) {
+        Livros livro=new Livros();
+        livro.setAutor("Ariano suassuna");
+        livro.setTitulo("auto da Compadecida");
+        livro.setAnoDePublicacao(1995);
+
+        Livros livro1=new Livros();
+        livro1.setAutor("J.K ROWLLING");
+        livro1.setTitulo("Harry Potter");
+        livro1.setAnoDePublicacao(2001);
+
+        
+        Livros livro2=new Livros();
+        livro2.setAutor("Dante");
+        livro2.setTitulo("A divina comédia");
+        livro2.setAnoDePublicacao(1843);
+
+        
+    }
+}
+
---
+++ Lista01/src/classes/Livros.java
@@ -0,0 +1,27 @@
+package classes;
+
+public class Livros {
+    private String autor;
+    private String titulo;
+    private int anoDePublicacao;
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getAnoDePublicacao() {
+        return anoDePublicacao;
+    }
+    public void setAnoDePublicacao(int anoDePublicacao) {
+        this.anoDePublicacao = anoDePublicacao;
+    }
+
+    
+}
